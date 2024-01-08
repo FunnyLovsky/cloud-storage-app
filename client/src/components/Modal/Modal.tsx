62e884b99d16c10eafb01d6da1b3f702de12e4f4
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-01-08 23:49:09 +0300

    fix front: modal, input

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -21,8 +21,24 @@ const Modal: FC = () => {
         setValue('')
     }
 
+    const keyDownHandler = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if(e.key === 'Enter') {
+            createDirHandler()
+        }
+    }
+
+    const closeModal = () => {
+        dispatch(actionsFile.setModal(false));
+        setValue('')
+    }
+
     return(
-        <div className={style.bg} style={{display: modal ? 'block' : 'none'}} onClick={() => dispatch(actionsFile.setModal(false))}>
+        <div 
+            className={style.bg} 
+            style={{display: modal ? 'block' : 'none'}} 
+            onClick={closeModal}
+            onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => keyDownHandler(e)}
+        >
             <Form onClick={true}>
                 <h3>Создать новую папку</h3>
                 <Input type='text' placeholder='Введите имя новой папки...' value={value} onChange={setValue}/>
